345d68baee174ef05be4ccd6d0a39cf42cf482c1
hurfy <hurfydev@gmail.com>
2023-12-21 02:03:58 +0300

    Initial commit

---
+++ config.py
@@ -0,0 +1,17 @@
+from decouple import config
+
+
+def telegram_token_validation() -> str:
+    """
+    Telegram Token Validation
+    :return: str: Telegram token
+    """
+    token = config('TELEGRAM_TOKEN')
+
+    if token.startswith('<') or not token:
+        raise Exception('Invalid or missing TELEGRAM_TOKEN in the .env file')
+
+    return token
+
+
+TELEGRAM_TOKEN = telegram_token_validation()
